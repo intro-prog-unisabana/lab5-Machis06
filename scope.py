76e23e73e49d7960c948390eec0f8a8a21bc6982
Mariana Tovar <marianatora@unisabana.edu.co>
2026-03-14 17:21:29 -0500

    lab 5

---
+++ scope.py
@@ -0,0 +1,9 @@
+def set_globals(some_int, some_str):
+    global global_int
+    global global_str
+    
+    global_int = some_int
+    global_str = some_str
+
+def get_globals():
+    return (global_int, global_str)
