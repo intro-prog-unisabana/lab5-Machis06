b52d05e26413a81c8f991f10440ae06ff7384351
Mariana Tovar <marianatora@unisabana.edu.co>
2026-03-14 16:24:39 -0500

    lab5

--- functions.py
+++ functions.py
@@ -1,13 +1,8 @@
-a = float(input(f"ingrese nota:"))
-b = float(input(f"ingrese nota:"))
-c = float(input(f"ingrese nota:"))
+def promedio_estudiante (calificaciones):
+    suma = sum(calificaciones)
+    promedio = suma/len(calificaciones)
+    return float(promedio)
 
-def promedio_estudiante (a, b, c):
-    suma = a+b+c
-    promedio = suma/3
-    return promedio
-resultado = promedio_estudiante(a,b,c)
-print(f"promedio: {resultado:.2f}")
 
 
 
