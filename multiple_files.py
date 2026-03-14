d3df3dcce5015af028c204c9f34c2fb6cae861dc
Mariana Tovar <marianatora@unisabana.edu.co>
2026-03-14 18:08:40 -0500

    lab 5

--- multiple_files.py
+++ multiple_files.py
@@ -2,5 +2,5 @@ from utils import *
 message = input("Please type your message\n")
 flipped_message = flip(message)
 num_a = count_letters(message, "a")
-encoded_message = flipped_message + ("a" * num_a)
-print("Your encoded message is:", encoded_message)
+encoded_message = flipped_message + "a" * num_a
+print(f"Your encoded message is:{encoded_message}")
