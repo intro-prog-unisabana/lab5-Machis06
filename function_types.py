ce5b9d09409af156b575742a7457b6adb3e785df
Mariana Tovar <marianatora@unisabana.edu.co>
2026-03-14 17:04:50 -0500

    lab 5

--- function_types.py
+++ function_types.py
@@ -1,9 +1,9 @@
-def list_shift (valores, shift):
-    for i in range(len(valores)):
-        valores[i] = valores[i]+shift 
+def list_shift (datos, shift):
+    for i in range(len(datos)):
+        datos[i] = datos[i]+shift 
 
-def calc_avg (valores):
-    return sum(valores)/len(valores)
+def calc_avg (datos):
+    return sum(datos)/len(datos)
 
-def print_normalized (valores):
-    print(valores)
+def print_normalized (datos):
+    print(datos)
